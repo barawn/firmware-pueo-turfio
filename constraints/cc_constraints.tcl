a08ddbf32209b7e33aced26d93b7dfa1ecf73d1f
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2023-06-26 21:34:40 -0400

    add more clock cross constraints

--- constraints/cc_constraints.tcl
+++ constraints/cc_constraints.tcl
@@ -17,19 +17,26 @@ set_max_delay -datapath_only -from $clockmon_run_reset_regs -to $clockmon_run_re
 set_max_delay -datapath_only -from $clockmon_run_regs -to $clockmon_run_cc_regs 10.000
 
 # the TURF module has a bajillion clock-crosses to deal with. This is our first set...
-set wb_static_regs [get_cells -hier -filter {NAME=~ *u_turf/*static_reg*}]
-set wb_static_targets [get_cells -hier -filter {NAME =~ *u_turf/u_cin_idelay*}]
-lappend wb_static_targets [get_cells -hier -filter {NAME=~ *u_turf/u_cin_idelay*}]
-lappend wb_static_targets [get_cells -hier -filter {NAME=~ *u_turf/u_rxclk_biterr/u_dsp}]
-lappend wb_static_targets [get_cells -hier -filter {NAME=~ *u_turf/cin_parallel_capture_reg*}]
-lappend wb_static_targets [get_cells -hier -filter {NAME=~ *u_turf/u_cin_iserdes*}]
+set wb_static_regs [get_cells -hier -filter {NAME=~ *u_turf/u_core/*static_reg*}]
+set wb_static_targets [get_cells -hier -filter {NAME =~ *u_turf/u_turfcin/u_cin_idelay*}]
+lappend wb_static_targets [get_cells -hier -filter {NAME=~ *u_turf/u_core/u_cin_biterr/u_dsp}]
+lappend wb_static_targets [get_cells -hier -filter {NAME=~ *u_turf/u_cin_sync/cin_capture*}]
+lappend wb_static_targets [get_cells -hier -filter {NAME=~ *u_turf/u_turfcin/u_cin_iserdes*}]
 set_max_delay -datapath_only -from $wb_static_regs -to $wb_static_targets 10.000
 
-set biterr_count_rxclk [get_cells -hier -filter {NAME=~ *u_turf/bit_error_count_reg*}]
-set biterr_count_wbclk [get_cells -hier -filter {NAME=~ *u_turf/bit_error_count_wbclk_reg*}]
+set biterr_count_rxclk [get_cells -hier -filter {NAME=~ *u_turf/u_core/u_cin_biterr/u_dsp}]
+set biterr_count_wbclk [get_cells -hier -filter {NAME=~ *u_turf/u_core/bit_error_count_wbclk_reg*}]
 set_max_delay -datapath_only -from $biterr_count_rxclk -to $biterr_count_wbclk 10.000
 
-set wb_dat_regs [get_cells -hier -filter {NAME=~ *u_turf/dat_reg_reg*}]
-set wb_dat_sources [get_cells -hier -filter {NAME=~ *u_turf/cin_parallel_capture_reg*}]
-lappend wb_dat_sources [get_cells -hier -filter {NAME=~ *u_turf/u_cin_idelay*}]
+set wb_dat_regs [get_cells -hier -filter {NAME=~ *u_turf/u_core/dat_reg_reg*}]
+set wb_dat_sources [get_cells -hier -filter {NAME=~ *u_turf/u_cin_sync/cin_capture*}]
+lappend wb_dat_sources [get_cells -hier -filter {NAME=~ *u_turf/u_turfcin/u_cin_idelay*}]
 set_max_delay -datapath_only -from $wb_dat_sources -to $wb_dat_regs 10.000
+
+# RXCLK/SYSCLK registers. Here we set a *min* delay. I should probably change this
+# and just set them up in an RLOC with some distance from each other. This does what
+# I want but it's not great because it still thinks it has to like, get from sysclk
+# to rxclk in 2.4 ns (which is *not* what I want, it's the *opposite* of what I want)
+set rxsys_xfr_src_regs [get_cells -hier -filter {CUSTOM_SYSCLK_SOURCE=="TRUE"}]
+set rxsys_xfr_tgt_regs [get_cells -hier -filter {CUSTOM_SYSCLK_TARGET=="TRUE"}]
+set_min_delay -from $rxsys_xfr_src_regs -to $rxsys_xfr_tgt_regs 0.0
