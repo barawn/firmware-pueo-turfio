7f3f58b75f4a142025c196013275e5ae1e852763
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-08-23 11:23:16 -0400

    parameterize the number of exported address bits and debug enable

--- hdl/aurora/aurora_wb_master.sv
+++ hdl/aurora/aurora_wb_master.sv
@@ -2,12 +2,10 @@
 // This module processes the Aurora command input streams,
 // executes the access, and returns the data. The command input stream
 // and response here comes via the UFC path.
-//
-// The commands can also be routed through the command processor
-// via the turfctl interface, but in that case the data will be returned
-// via the TURFIO's COUT path.
 `include "interfaces.vh"
-module aurora_wb_master(
+// ADDR_BITS here can only be up to 31
+module aurora_wb_master #(parameter ADDR_BITS=25,
+                          parameter DEBUG = "FALSE")(
         // NOTE: aclk = wbclk
         input aclk,
         input aresetn,
@@ -15,7 +13,7 @@ module aurora_wb_master(
         `TARGET_NAMED_PORTS_AXI4S_MIN_IF( s_data_ , 32 ),
         `HOST_NAMED_PORTS_AXI4S_MIN_IF( m_resp_ , 32 ),
         input wb_rst_i,        
-        `HOST_NAMED_PORTS_WB_IF( wb_ , 22, 32 )        
+        `HOST_NAMED_PORTS_WB_IF( wb_ , ADDR_BITS, 32 )        
     );
     
     localparam FSM_BITS=2;
@@ -55,12 +53,23 @@ module aurora_wb_master(
         
         if (wb_ack_i) resp_data <= wb_dat_i;
     end
-    
+
+    generate
+        if (DEBUG == "TRUE") begin : ILA
+            aurora_cmd_ila u_acmd_ila(.clk(aclk),
+                                 .probe0( s_addr_tdata ),
+                                 .probe1( s_addr_tvalid ),
+                                 .probe2( s_data_tdata ),
+                                 .probe3( s_data_tvalid ),
+                                 .probe4( m_resp_tdata ),
+                                 .probe5( m_resp_tvalid) );
+        end
+    endgenerate        
     // now all of these should just be direct paths
     assign s_addr_tready = addr_tready;
     assign s_data_tready = data_tready;
     
-    assign wb_adr_o = {s_addr_tdata[2 +: 20], 2'b00};
+    assign wb_adr_o = {s_addr_tdata[2 +: (ADDR_BITS-2)], 2'b00};
     assign wb_we_o = !s_addr_tdata[31];
     assign wb_sel_o = {4{wb_we_o}};
     assign wb_dat_o = s_data_tdata;        
