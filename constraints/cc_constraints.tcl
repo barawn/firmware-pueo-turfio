e2ac495dcf97ac253e5ae135e5848b62fcd4428e
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-08-23 11:21:43 -0400

    move dbg_hub clock hookup to tcl so it can be guarded

--- constraints/cc_constraints.tcl
+++ constraints/cc_constraints.tcl
@@ -151,3 +151,14 @@ set_cc_paths $sysclk $initclk $clktypelist
 
 set_cc_paths $userclk $initclk $clktypelist
 set_cc_paths $initclk $userclk $clktypelist
+
+set hub [get_debug_cores dbg_hub -quiet]
+if { [llength $hub] > 0 } {
+    set_property C_CLK_INPUT_FREQ_HZ 300000000 [get_debug_cores dbg_hub]
+    set_property C_ENABLE_CLK_DIVIDER false [get_debug_cores dbg_hub]
+    set_property C_USER_SCAN_CHAIN 1 [get_debug_cores dbg_hub]
+    connect_debug_port dbg_hub/clk [get_nets u_clk200/inst/clk_out2_clk200_wiz]
+} else {
+    puts "skipping debug hub commands, not inserted yet"
+}
+    
