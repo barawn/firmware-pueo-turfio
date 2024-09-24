efdb145e7e8959de3b31e52106bc5eb737ce92bf
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-09-24 15:38:26 -0400

    add splicer and surfturf register core

---
+++ hdl/surfturf/surfturf_register_core.sv
@@ -0,0 +1,119 @@
+`timescale 1ns / 1ps
+`include "interfaces.vh"
+`include "rackbus.vh"
+// This might end up being temporary, I don't know.
+// The fw_ path needs to be spliced with the mode1_ data as well,
+// and it might need to be spliced with actual fw_ data from
+// Aurora.
+// But I need to be able to Test Stuff Now, so this is what it is.
+module surfturf_register_core #(parameter WB_CLK_TYPE = "NONE")(
+        input wb_clk_i,
+        input wb_rst_i,
+        // 10 bit interface, taking 0x400-0x7FF.
+        // we likely won't need anywhere NEAR that
+        `TARGET_NAMED_PORTS_WB_IF(wb_ , 10, 32),
+        input sysclk_i,
+        // FW update output port
+        `HOST_NAMED_PORTS_AXI4S_MIN_IF( fw_ , 8),
+        `HOST_NAMED_PORTS_AXI4S_MIN_IF( runcmd_ , `RACKBUS_RUNCMD_BITS ),
+        `HOST_NAMED_PORTS_AXI4S_MIN_IF( trig_ , `RACKBUS_TRIG_BITS )       
+    );
+    
+    // register 0: control and FIFO status (no FIFOs for runcmd/trig!)
+    // register 1: fwupdate register (32-bits, reshaped to 8 bits in sysclk domain
+    // register 2: runcmd register (only 2 bits)
+    // register 3: trigger register (only 15 bits)
+    localparam [3:0] CONTROL_ADDR = 4'h0;
+    localparam [3:0] FWUPDATE_ADDR = 4'h4;
+    localparam [3:0] RUNCMD_ADDR = 4'h8;
+    localparam [3:0] TRIG_ADDR = 4'hC;
+    
+    (* CUSTOM_CC_SRC = WB_CLK_TYPE *)
+    reg [`RACKBUS_RUNCMD_BITS-1:0] runcmd_holding_reg = {`RACKBUS_RUNCMD_BITS{1'b0}};
+    (* CUSTOM_CC_SRC = "SYSCLK" *)
+    reg runcmd_holding_valid = 0;
+    wire runcmd_is_valid_wbclk = (wb_cyc_i && wb_stb_i && wb_ack_o && (wb_adr_i[3:0] == RUNCMD_ADDR) && wb_we_i && wb_sel_i[0]);
+    wire runcmd_is_valid_sysclk;
+    (* CUSTOM_CC_DST = WB_CLK_TYPE, ASYNC_REG = "TRUE" *)
+    reg [1:0] runcmd_holding_valid_wbclk = {2{1'b0}};
+    (* CUSTOM_CC_SRC = "SYSCLK" *)
+    reg [`RACKBUS_TRIG_BITS-1:0] trig_holding_reg = {`RACKBUS_TRIG_BITS{1'b0}};
+    (* CUSTOM_CC_SRC = "SYSCLK" *)
+    reg trig_holding_valid = 0;
+    wire trig_is_valid_wbclk = (wb_cyc_i && wb_stb_i && wb_ack_o && (wb_adr_i[3:0] == TRIG_ADDR) && wb_we_i && (wb_sel_i[0] && wb_sel_i[1]));
+    wire trig_is_valid_sysclk;
+    (* CUSTOM_CC_DST = WB_CLK_TYPE, ASYNC_REG = "TRUE" *)
+    reg [1:0] trig_holding_valid_wbclk = {2{1'b0}};
+    
+    wire fw_write = (wb_cyc_i && wb_stb_i && wb_ack_o && (wb_adr_i[3:0] == FWUPDATE_ADDR) && wb_we_i);
+    wire fw_empty;
+    (* CUSTOM_CC_SRC = "SYSCLK" *)
+    reg fw_empty_sysclk = 0;
+    (* CUSTOM_CC_DST = WB_CLK_TYPE, ASYNC_REG = "TRUE" *)
+    reg [1:0] fw_empty_wbclk = {2{1'b0}};
+
+    reg ack = 0;
+    reg fwupdate_fifo_reset = 0;
+    
+    assign wb_ack_o = ack && wb_cyc_i;
+    // I don't hook up full here because we know how much to write:
+    // you get 1024 integers to write, period, then you check until empty.
+    // we can speed this up in the future by letting fwupdate data be the stuff
+    // that comes from the TURF Aurora directory and implementing flow control.
+    // then the TURF side can buffer a lot more and blast it out.    
+    // note that you can mask off SURFs by just not putting them in update mode.
+    fwupdate_fifo u_fifo(.wr_clk(wb_clk_i),
+                         .rd_clk(sysclk_i),
+                         .rst(fwupdate_fifo_reset),
+                         .din(wb_dat_i),
+                         .wr_en(fw_write),
+                         .empty(fw_empty),
+                         .dout(fw_tdata),
+                         .valid(fw_tvalid),
+                         .rd_en(fw_tvalid && fw_tready));
+
+    flag_sync u_runcmd_valid(.in_clkA(runcmd_is_valid_wbclk),.out_clkB(runcmd_is_valid_sysclk),
+                             .clkA(wb_clk_i),.clkB(sysclk_i));
+    flag_sync u_trig_valid(.in_clkA(trig_is_valid_wbclk),.out_clkB(trig_is_valid_sysclk),
+                           .clkA(wb_clk_i),.clkB(sysclk_i));                             
+
+    always @(posedge sysclk_i) begin
+        if (runcmd_tready) runcmd_holding_valid <= 1'b0;
+        else if (runcmd_is_valid_wbclk) runcmd_holding_valid <= 1'b1;
+        
+        if (trig_tready) trig_holding_valid <= 1'b0;
+        else if (trig_is_valid_wbclk) trig_holding_valid <= 1'b1;
+        
+        fw_empty_sysclk <= fw_empty;
+    end
+    
+    always @(posedge wb_clk_i) begin        
+        fw_empty_wbclk <= {fw_empty_wbclk[0], fw_empty_sysclk};
+
+        ack <= wb_cyc_i && wb_stb_i;
+        
+        if (wb_cyc_i && wb_stb_i && wb_we_i && wb_adr_i[3:0] == CONTROL_ADDR && wb_sel_i[0])
+            fwupdate_fifo_reset <= wb_dat_i[0];
+
+        // this is probably batshit stupid: it won't stay set that long so checking if it's zero
+        // will probably just get you zero right away        
+        runcmd_holding_valid_wbclk <= { runcmd_holding_valid_wbclk[0], runcmd_holding_valid };
+        trig_holding_valid_wbclk <= { trig_holding_valid_wbclk[0], trig_holding_valid };
+        
+        if (runcmd_is_valid_wbclk)
+            runcmd_holding_reg <= wb_dat_i[0 +: `RACKBUS_RUNCMD_BITS];
+        if (trig_is_valid_wbclk)
+            trig_holding_reg <= wb_dat_i[0 + `RACKBUS_TRIG_BITS];                                
+    end
+    
+    assign runcmd_tvalid = runcmd_holding_valid;
+    assign runcmd_tdata = runcmd_holding_reg;
+    
+    assign trig_tvalid = trig_holding_valid;
+    assign trig_tdata = trig_holding_reg;
+    
+    assign wb_err_o = 1'b0;
+    assign wb_rty_o = 1'b0;
+    assign wb_dat_o = { {28{1'b0}}, !trig_holding_valid_wbclk[1], !runcmd_holding_valid_wbclk[1], fw_empty_wbclk[1], fwupdate_fifo_reset };
+    
+endmodule
--- hdl/surfturf_wrapper_v2.sv
+++ hdl/surfturf_wrapper_v2.sv
@@ -1,5 +1,6 @@
 `timescale 1ns / 1ps
 `include "interfaces.vh"
+`include "rackbus.vh"
 // this thing is a mess, sigh
 module surfturf_wrapper_v2 #(
         parameter T_RXCLK_INV = 1'b0,
@@ -26,11 +27,19 @@ module surfturf_wrapper_v2 #(
         input rxclk_ok_i,
         output rxclk_o,
         output rxclk_x2_o,
+        // TURFIO input paths. These get spliced in,
+        `TARGET_NAMED_PORTS_AXI4S_MIN_IF( mode1_ , 8 ),
+        input [1:0] mode1_tuser,
+        // local TURFIO PPS, and its enable
+        input tfio_pps_i,
+        // this needs to go EVERYWHERE we swap between PPSes!!!
+        input use_tfio_pps_i,        
+        
         // turf outputs
         output          command_locked_o,
         output [31:0]   command_o,
         output          command_valid_o,
-        // response inputs
+        // response inputs, I don't know if I'll ever use this
         input [31:0]    response_i,
         
         // turf-side
@@ -58,17 +67,62 @@ module surfturf_wrapper_v2 #(
     
     localparam [6:0] SURF_DEBUG = 7'b0000011;
     
+    // our address space is 12 bits
+    // we give each module a 6-bit space (64 bytes = 16 32-bit registers).
+    // that takes up 8 bits of the address space
+    // also slice off the top bit and use it as a debug space for the spliced
+    // command crap: that way it doesn't have to come from outside.
+    
     // create a vector of WB interfaces to use.
-    `DEFINE_WB_IFV( wbvec_ , 12, 32, [7:0] );
+    `DEFINE_WB_IFV( wbvec_ , 6, 32, [7:0] );
+    // and our surfturf interface
+    `DEFINE_WB_IF( surfturf_ , 10, 32 );
+    // splice in the surfturf
+    assign surfturf_cyc_o = wb_cyc_i;
+    assign surfturf_stb_o = wb_stb_i && wb_adr_i[11];
+    assign surfturf_adr_o = wb_adr_i[9:0];
+    assign surfturf_we_o = wb_we_i;
+    assign surfturf_sel_o = wb_sel_i;
+    assign surfturf_dat_o = wb_dat_i;
     
     wire [27:0] surf_response;
     
+    // spliced command
+    wire [31:0] surf_command;
+
+    // internal stuff
+    `DEFINE_AXI4S_MIN_IF( tfio_runcmd_ , 2);
+    `DEFINE_AXI4S_MIN_IF( tfio_trig_ , 15);
+    `DEFINE_AXI4S_MIN_IF( tfio_fw_ , 8);
+    
+    surfturf_register_core #(.WB_CLK_TYPE(WB_CLK_TYPE))
+            u_st_core(.wb_clk_i(wb_clk_i),
+                      .wb_rst_i(wb_rst_i),
+                      `CONNECT_WBS_IFM( wb_ , surfturf_ ),
+                      .sysclk_i(sysclk_i),
+                      `CONNECT_AXI4S_MIN_IF( fw_ , tfio_fw_ ),
+                      `CONNECT_AXI4S_MIN_IF( runcmd_ , tfio_runcmd_ ),
+                      `CONNECT_AXI4S_MIN_IF( trig_ , tfio_trig_ ));                                    
+
+    turfio_cmd_splice u_splice(.sysclk_i(sysclk_i),
+                               .sync_i(sync_i),
+                               .command_i(command_o),
+                               .command_valid_i(command_valid_o),
+                               `CONNECT_AXI4S_MIN_IF( mode1_ , mode1_ ),
+                               .mode1_tuser(mode1_tuser),
+                               `CONNECT_AXI4S_MIN_IF( tfio_runcmd_ , tfio_runcmd_ ),
+                               `CONNECT_AXI4S_MIN_IF( tfio_trig_ , tfio_trig_ ),
+                               `CONNECT_AXI4S_MIN_IF( tfio_fw_ , tfio_fw_ ),
+                               .tfio_pps_i(tfio_pps_i),
+                               .use_tfio_pps_i(use_tfio_pps_i),
+                               .spliced_o(surf_command));
+        
     generate
         genvar i;
         for (i=0;i<8;i=i+1) begin : LP
             assign wbvec_cyc_o[ i ] = wb_cyc_i;
-            assign wbvec_stb_o[ i ] = wb_stb_i && (wb_adr_i[6 +: 3] == i);
-            assign wbvec_adr_o[ i ] = wb_adr_i;
+            assign wbvec_stb_o[ i ] = wb_stb_i && (wb_adr_i[6 +: 3] == i) && !wb_adr_i[11];
+            assign wbvec_adr_o[ i ] = wb_adr_i[5:0];
             assign wbvec_sel_o[ i ] = wb_sel_i;
             assign wbvec_dat_o[ i ] = wb_dat_i;
             assign wbvec_we_o[ i ] = wb_we_i;
@@ -122,13 +176,11 @@ module surfturf_wrapper_v2 #(
                             .sync_i(sync_i),
                             // sigh I have no idea if I'm going to do it
                             // this way or not BUT FOR NOW!
-                            .command_i(command_o),
+                            .command_i(surf_command),
                             .COUT_P(COUT_P[i-1]),
                             .COUT_N(COUT_N[i-1]),
                             .DOUT_P(DOUT_P[i-1]),
                             .DOUT_N(DOUT_N[i-1]),
-//                            .TXCLK_P(TXCLK_P[i-1]),
-//                            .TXCLK_N(TXCLK_N[i-1]),
                             .CIN_P(CIN_P[i-1]),
                             .CIN_N(CIN_N[i-1]),
                             .RXCLK_P(RXCLK_P[i-1]),
@@ -138,8 +190,8 @@ module surfturf_wrapper_v2 #(
     endgenerate    
 
     // hopefully this works, it's a pretty big mux, we'll see
-    assign wb_ack_o = wbvec_ack_i[ wb_adr_i[6 +: 3] ];
-    assign wb_dat_o = wbvec_dat_i[ wb_adr_i[6 +: 3] ];
+    assign wb_ack_o = wb_adr_i[11] ? surfturf_ack_i : wbvec_ack_i[ wb_adr_i[6 +: 3] ];
+    assign wb_dat_o = wb_adr_i[11] ? surfturf_dat_i : wbvec_dat_i[ wb_adr_i[6 +: 3] ];
     assign wb_err_o = 1'b0;
     assign wb_rty_o = 1'b0;
 endmodule
