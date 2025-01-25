68acb8bb6e4734eedc4056906c8bebe379d93c43
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-01-25 14:39:29 -0500

    sooo much more testing added, still need eEnable, ePMBus read, and eStatistics

--- sim/turfio_hski2c_tb.sv
+++ sim/turfio_hski2c_tb.sv
@@ -7,6 +7,12 @@ module turfio_hski2c_tb;
     pullup(sda);
     wire scl;
     pullup(scl);
+
+    wire sda_i, sda_t;
+    wire scl_i, scl_t;    
+    IOBUF u_sda(.T(sda_t),.IO(sda),.I(1'b0),.O(sda_i));
+    IOBUF u_scl(.T(scl_t),.IO(scl),.I(1'b0),.O(scl_i));
+
     wire I2C_RDY = 1'b1;
 
     // this is SURF4
@@ -50,26 +56,90 @@ module turfio_hski2c_tb;
                     .buffer_data_present(tx_data_present),
                     .data_in(uart_data),
                     .serial_out(HSK_RX));
+    // ok we need to start automating this stuff
+    wire rx_has_data;
+    wire [7:0] rx_data;
+    uart_rx6 u_rx(.clk(clk),.en_16_x_baud(en_16x_baud),
+                      .buffer_read(rx_has_data),
+                      .buffer_data_present(rx_has_data),
+                      .data_out(rx_data),
+                      .serial_in(HSK_TX));
+    reg [7:0] packet[255:0];
+    reg [7:0] packet_pointer = {8{1'b0}};
+    reg [7:0] zero_counter = {8{1'b0}};
+    always @(posedge clk) begin
+        if (rx_has_data) begin
+            $display("received data %x", rx_data);
+            if (zero_counter == 0) begin
+                // start of packet
+                zero_counter <= rx_data;
+                packet_pointer <= 0;
+            end else if (zero_counter == 1) begin
+                // encoded zero hit. if zero, is end of packet
+                if (rx_data == 8'h00) begin
+                    integer i;
+                    $display("got packet:");
+                    for (i=0;i<packet_pointer;i=i+1) begin
+                        $write("%x", packet[i]);
+                        if (i == packet_pointer-1) begin
+                            $display("");
+                        end else begin
+                            $write(" ");
+                        end                            
+                    end
+                    zero_counter <= 0;
+                end else begin // otherwise this is a real zero
+                    zero_counter <= rx_data;
+                    packet[packet_pointer] <= {8{1'b0}};
+                    packet_pointer <= packet_pointer + 1;
+                end // if rx_data != 0
+            end else begin //if zero_counter != 0 and != 1
+                packet[packet_pointer] <= rx_data;
+                packet_pointer <= packet_pointer + 1;
+                zero_counter <= zero_counter - 1;
+            end
+        end // if rx_has_data
+    end // always
+    
+    reg cyc = 0;
+    reg we = 0;
+    reg [31:0] dat_i = {32{1'b0}};
+    wire [31:0] dat_o;
+    wire ack;
     
     hski2c_top #(.SIM_FAST("TRUE")) uut(.wb_clk_i(clk),
-                   .wb_rst_i(rst),
-                   .wb_cyc_i(1'b0),
-                   .wb_stb_i(1'b0),
-                   .wb_we_i(1'b0),
-                   .wb_sel_i({4{1'b0}}),
-                   .wb_dat_i({32{1'b0}}),
+                   .wb_rst_i(1'b0),
+                   .wb_cyc_i(cyc),
+                   .wb_stb_i(cyc),
+                   .wb_we_i(we),
+                   .wb_sel_i({4{we}}),
+                   .wb_dat_i(dat_i),
                    .wb_adr_i({12{1'b0}}),
+                   .wb_ack_o(ack),
+                   .wb_dat_o(dat_o),
                    .CONF(2'b00),
-                   .F_SDA(sda),
-                   .F_SCL(scl),
+                   .sda_i(sda_i),
+                   .sda_t(sda_t),
+                   .scl_i(scl_i),
+                   .scl_t(scl_t),
                    .HSK_TX(HSK_TX),
                    .HSK_RX(HSK_RX),
                    .I2C_RDY(I2C_RDY));
 
     initial begin
         #100;
-        @(posedge clk); #1 rst <= 0;
+        @(posedge clk); #1 rst = 0;
+        @(posedge clk);
+        #1 cyc = 1; dat_i = 32'd1; we = 1;
+        while (!ack) @(posedge clk); 
+        #1 cyc = 0; we = 0;
 
+        #100;
+        @(posedge clk);
+        #1 cyc = 1; dat_i = 32'd0; we = 1;
+        while (!ack) @(posedge clk);
+        #1 cyc = 0; we = 0;
+        
         // we need to wait STUPID LONG because pre initialization it will just throw away
         // our data, bastards
         #1000000;
@@ -100,7 +170,54 @@ module turfio_hski2c_tb;
         #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
         #1 uart_data = 8'h00; uart_write = 1; @(posedge clk);
         #1 uart_write = 0; @(posedge clk);
-        
+        #1000000;
+        @(posedge clk);
+        // eTemps from 00 to 40: 00 40 10 00 00
+        // COBS               01 03 40 10 01 01 00
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h03; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h40; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h10; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h00; uart_write = 1; @(posedge clk);
+        #1 uart_write = 0; @(posedge clk);
+        #1000000;
+        @(posedge clk);
+        // eIdentify from 00 to 40: same as above just with 12
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h03; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h40; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h12; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h00; uart_write = 1; @(posedge clk);
+        #1 uart_write = 0; @(posedge clk);
+        #1000000;
+        // eCurrents from 00 to 40: same as above just with 13
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h03; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h40; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h13; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h00; uart_write = 1; @(posedge clk);
+        #1 uart_write = 0; @(posedge clk);
+        #1000000;
+    
+        // ePMBus from 00 to 40 sending 0x80 0xD9
+        //    00 40 C1 02 80 D9 A7
+        // 01 07 40 C1 02 80 D9 A7 00
+        #1 uart_data = 8'h01; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h07; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h40; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'hC1; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h02; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h80; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'hD9; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'hA7; uart_write = 1; @(posedge clk);
+        #1 uart_data = 8'h00; uart_write = 1; @(posedge clk);
+        #1 uart_write = 0; @(posedge clk);
     end
                    
 endmodule
