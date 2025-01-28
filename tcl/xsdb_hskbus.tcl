7cc393a841447b42b286d672bcb07870b7837657
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-01-28 15:52:50 -0500

    add hskbus script for xsdb

---
+++ tcl/xsdb_hskbus.tcl
@@ -0,0 +1,21 @@
+proc hskbus_enable { } {
+    jtag targets -set -filter {name =~ "xc7a35t"}
+    set jseq [jtag sequence]
+    # shift 6-bit IR code of 0x23 in (USER4)
+    $jseq irshift -state IDLE -hex 6 23
+    # shift 8 bit USER4 code in (0x80, enable HSKBUS)
+    $jseq drshift -state IDLE -hex 8 80
+    $jseq run
+    $jseq clear
+}
+
+proc hskbus_disable { } {
+    jtag targets -set -filter {name =~ "xc7a35t"}
+    set jseq [jtag sequence]
+    # shift 6-bit IR code of 0x23 in (USER4)
+    $jseq irshift -state IDLE -hex 6 23
+    # shift 8 bit USER4 code in (0x00, disable HSKBUS)
+    $jseq drshift -state IDLE -hex 8 00
+    $jseq run
+    $jseq clear
+}
