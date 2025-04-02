74c87b81258d9aa2d744e144ec658f651f7ebe22
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-04-02 11:19:33 -0400

    add force crate enable

--- tcl/xsdb_hskbus.tcl
+++ tcl/xsdb_hskbus.tcl
@@ -24,6 +24,21 @@ proc hskbus_enable_full { args } {
     $jseq clear
 }
 
+# force the SURF crate on
+proc hskbus_enable_force_crate { args } {
+    array set data [list -sn "*" {*}$args]
+    set sn $data(-sn)
+    jtag targets -set -filter {name =~ "xc7a35t" && jtag_cable_serial =~ $sn }
+    set jseq [jtag sequence]
+    # shift 6-bit IR code of 0x23 in (USER4)
+    $jseq irshift -state IDLE -hex 6 23
+    # shift 8 bit USER4 code in (0x40, ONLY enable cratebridge)
+    $jseq drshift -state IDLE -hex 8 00
+    $jseq run
+    $jseq clear
+}
+
+# disable everything
 proc hskbus_disable { args } {
     array set data [list -sn "*" {*}$args]
     set sn $data(-sn)
@@ -36,3 +51,5 @@ proc hskbus_disable { args } {
     $jseq run
     $jseq clear
 }
+
+
